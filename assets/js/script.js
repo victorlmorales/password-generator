4a9460ccf579c83327ae8e961dd7e3a6d4adbb37
victorlmorales <victor.l.morales2020@gmail.com>
2022-11-09 18:45:16 -0600

    Added criteria variables to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCaseLetters = lowerCaseLetters.map(function(letters) {
+    return letters.toUpperCase();
+    }
+  )
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+  var emptySpace = [];
+  
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
